ff5783fbc1b52e8307862c9e1cf28c12b3f7e865
paribesh10 <62760565+paribesh10@users.noreply.github.com>
2022-03-17 01:36:02 +0000

    some bug fixed

--- main.py
+++ main.py
@@ -12,13 +12,10 @@ print("Welcome to Treasure Island.")
 print("Your mission is to find the treasure.") 
 
 choice1 = input('You\'re at a cross road. Where do you want to go? Type "left" or "right" \n').lower()
-if choice1 == "right":
-  print("You fell into a hole. Game Over.")
-else:
+
+if choice1 == "left":
   choice2 = input('You have come to a sea. There is an island in the middle of the lake.Type "wait" to wait for a boat. Type "swim" to swim across \n').lower()
-  if choice2 == "swim":
-    print("There are some sharks in the sea. You can not pass. Game Over.")
-  else:
+  if choice2 == "wait":
     choice3 = input('You have arrived at the island safely. There is a house of three doors. Red, yellow and blue. Which colour do you chooose? \n').lower()
     if choice3 == "yellow":
       print("CONGRATULTIONS!!! You find a treasure.")
@@ -48,6 +45,8 @@ else:
       print("Eaten by beasts. Game Over.")
     else:
       print("Game Over")
-  
-  
+  else:
+    print("There are some sharks in the sea. You can not pass. Game Over.")
+else:
+  print("You fell into a hole. Game Over.")  
 
