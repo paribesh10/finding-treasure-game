4f65ad46aeab38096ba5176331b9b366d7394a81
paribesh10 <62760565+paribesh10@users.noreply.github.com>
2022-03-17 01:20:37 +0000

    removed comment

--- main.py
+++ main.py
@@ -11,9 +11,6 @@ Y88b. 888    Y8b.    888  888     X88Y88b 888888    Y8b.
 print("Welcome to Treasure Island.")
 print("Your mission is to find the treasure.") 
 
-#https://www.draw.io/?lightbox=1&highlight=0000ff&edit=_blank&layers=1&nav=1&title=Treasure%20Island%20Conditional.drawio#Uhttps%3A%2F%2Fdrive.google.com%2Fuc%3Fid%3D1oDe4ehjWZipYRsVfeAx2HyB7LCQ8_Fvi%26export%3Ddownload
-
-#Write your code below this line 👇
 choice1 = input('You\'re at a cross road. Where do you want to go? Type "left" or "right" \n').lower()
 if choice1 == "right":
   print("You fell into a hole. Game Over.")
